4e0a25565ac488a35b846fc0f1dfd2fd3d81d5bf
Mohammad ABDUL <abdul.opto@gmail.com>
2025-01-22 10:58:59 +0100

    Ajout pagination dans le controller et la vue

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -3,19 +3,26 @@
 namespace App\Controller;
 
 use App\Repository\TripRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TripController extends AbstractController
 {
     #[Route('/trips', name: 'app_trips', methods: ['GET'])]
-    public function index(TripRepository $tR): Response
+    public function index(TripRepository $tR, PaginatorInterface $paginator, Request $request): Response
     {
+
+        $pagination = $paginator->paginate(
+            $tR->findAll(), 
+            $request->query->getInt('page', 1), /* page number */
+            18 /* limit per page */
+        );
+
         return $this->render('trip/index.html.twig', [
-            'trips' => $tR->findBy(
-                [], ['id'=>'ASC']
-            ),
+            'trips' => $pagination,
             'title' => 'Trips',
             'description' => 'Les trips disponibles sur la plateforme. 100% made by you !',
         ]);
